21700bf7f05e3dd0eb4099594806ffff674c5ee0
Shemona Singh <shemonasingh@Shemonas-MacBook-Pro.local>
2018-03-07 16:46:30 -0500

    completed project, rep made

---
+++ app/src/main/java/com/singh/patientdashboard/PatientDashboard.java
@@ -0,0 +1,56 @@
+package com.singh.patientdashboard;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class PatientDashboard extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_stats:
+                    mTextMessage.setText(R.string.title_stats);
+                    return true;
+                case R.id.navigation_bundles:
+                    mTextMessage.setText(R.string.title_bundles);
+                    return true;
+                case R.id.navigation_chat:
+                    mTextMessage.setText(R.string.title_chat);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_dashboard);
+
+
+    }
+
+
+
+
+}
